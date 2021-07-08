cc59632cf7761e2c55472095d128b15410524e22
highcowoo <highcowoo@gmail.com>
2021-07-08 21:34:49 +0900

    Initial commit

---
+++ resources/views/header.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="main.css" />
+    <title>Document</title>
+</head>
+<body>
+
+        <div id="header" class="header">
+    <div id="navi" class="Naviation">
+ 
+            <a  class="Logo" href="">에브리팀</a>
+            <a  class="Loginbutton" href="">Login</a>
+  
+    </div>
+    <div id="searchlayout" class="searchl">
+    <INPUT type="text" name="Searchtext" placeholder="검색어를 입력하세요" class="search">
+    <a  class="searchbutton" href="">Search</a>
+    </div>
+    <div id="menul" class="menul">
+        
+               <input type="button" class="menu first" onclick="location.href='aa.php'" value="스터디">
+               <input type="button" class="menu" onclick="location.href='aa.php'" value="스포츠">
+               <input type="button" class="menu" onclick="location.href='aa.php'" value="동아리">
+               <input type="button" class="menu" onclick="location.href='aa.php'" value="게임">
+               <input type="button" class="menu" onclick="location.href='aa.php'" value="팀만들기">
+            
+        
+        
+    </div>
+</div>
+
+</body>
+</html>
